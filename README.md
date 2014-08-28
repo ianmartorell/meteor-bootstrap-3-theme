fee0ade002addac23bceadecdafac55b50e7508d
Ian Martorell <ianmartorell@gmail.com>
2014-08-28 20:51:29 +0200

    Removed unused files & updated README

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 Meteor bootstrap-3-theme
 ==================
 
-Provides the Bootstrap Theme from [Bootstrap 3.2](http://getbootstrap.com) on your [Meteor](http://meteor.com) app.
-
+Provides the Bootstrap Theme from [Bootstrap 3.2](http://getbootstrap.com) to your [Meteor](http://meteor.com) app.
 
 ### Installation
 
@@ -12,4 +11,4 @@ With Meteor 0.9:
 $ meteor add ian:bootstrap-3-theme
 ```
 
-Look [here](https://github.com/ianmartorell/meteor-bootstrap-3) for the non-theme version of Bootstrap 3 for Meteor.
+This package uses [ian:bootstrap-3](https://github.com/ianmartorell/meteor-bootstrap-3) for the base Bootstrap styles.
--- package.js
+++ package.js
@@ -1,19 +1,15 @@
 Package.describe({
   name: 'ian:bootstrap-3-theme',
   summary: 'The Bootstrap Theme from Bootstrap 3.2',
-  version: '1.0.0',
+  version: '1.0.1',
   git: "https://github.com/ianmartorell/meteor-bootstrap-3-theme"
 });
 
 Package.on_use(function (api) {
-  api.use('jquery@1.0.0');
+  api.use([
+    'jquery@1.0.0',
+    'ian:bootstrap-3@1.0.2',
+    ], 'client');
 
   api.add_files('css/bootstrap-theme.min.css', 'client');
-  api.add_files('js/bootstrap.min.js', 'client');
-
-  // Fonts
-  api.add_files('fonts/glyphicons-halflings-regular.eot', 'client');
-  api.add_files('fonts/glyphicons-halflings-regular.ttf', 'client');
-  api.add_files('fonts/glyphicons-halflings-regular.svg', 'client');
-  api.add_files('fonts/glyphicons-halflings-regular.woff', 'client');
 });
