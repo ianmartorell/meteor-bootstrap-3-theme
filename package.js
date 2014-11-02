94860930ff11e70e7c64c26d7d1fdf7eec946564
Ian Martorell <ianmartorell@gmail.com>
2014-11-02 15:04:58 +0100

    Updated to Bootstrap 3.3

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'ian:bootstrap-3-theme',
-  summary: 'The Bootstrap Theme from Bootstrap 3.2',
-  version: '1.0.1',
+  summary: 'The Bootstrap Theme from Bootstrap 3.x',
+  version: '3.3.0_1',
   git: "https://github.com/ianmartorell/meteor-bootstrap-3-theme"
 });
 
 Package.on_use(function (api) {
   api.use([
     'jquery@1.0.0',
-    'ian:bootstrap-3@1.0.2',
+    'ian:bootstrap-3@3.3.0_2',
     ], 'client');
 
-  api.add_files('css/bootstrap-theme.min.css', 'client');
+  api.add_files('css/bootstrap-theme.css', 'client');
 });
